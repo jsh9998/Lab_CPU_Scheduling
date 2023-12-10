1641f9b60a43d3b6b9043be06d069dd40134699b
ProgramHenry <jsh9998@nyu.edu>
2023-12-09 19:12:56 -0600

    test
    test

--- cpu.c
+++ cpu.c
@@ -31,7 +31,7 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
             new_process.execution_endtime = 0;
             new_process.remaining_bursttime = new_process.total_bursttime;
             ready_queue[(*queue_cnt)] = new_process;
-            (*queue_cnt)++;
+            *queue_cnt = *queue_cnt + 1;
 
             return current_process;
         }
@@ -42,7 +42,7 @@ struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queu
             new_process.remaining_bursttime = new_process.total_bursttime;
             current_process.execution_endtime = 0;
             ready_queue[(*queue_cnt)] = current_process;
-            (*queue_cnt)++;
+            *queue_cnt = *queue_cnt + 1;
 
             return new_process;
         }
